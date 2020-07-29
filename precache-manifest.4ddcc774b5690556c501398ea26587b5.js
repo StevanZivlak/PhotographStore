157ab970dbcc8d5ae735fd594e13f4366acbfc42
StevanZivlak <zivlaks@yahoo.com>
2020-07-29 11:08:24 +0200

    Updates

---
+++ precache-manifest.4ddcc774b5690556c501398ea26587b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "565bccd668e297d97ea1aa3abbeacd42",
+    "url": "/PhotographStore/index.html"
+  },
+  {
+    "revision": "6a8514bd592147851090",
+    "url": "/PhotographStore/static/css/main.eb8bb08b.chunk.css"
+  },
+  {
+    "revision": "9a6de4a5b5df144a1229",
+    "url": "/PhotographStore/static/js/2.6b653f55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PhotographStore/static/js/2.6b653f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a8514bd592147851090",
+    "url": "/PhotographStore/static/js/main.9582c8bf.chunk.js"
+  },
+  {
+    "revision": "6ad94707222b1f9d23a3",
+    "url": "/PhotographStore/static/js/runtime-main.54fee822.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.258ca2dfa7fd493a1a9d316720fb6152.js"
+  "/PhotographStore/precache-manifest.4ddcc774b5690556c501398ea26587b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PhotographStore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
